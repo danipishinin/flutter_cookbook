859a6d5491b7eef52e7f2b5be324a704f988277b
Daniele Pishinin <danipishiniN@gmail.com>
2021-01-23 14:42:06 -0300

    settings - screen

--- cookbook/lib/src/views/settings.dart
+++ cookbook/lib/src/views/settings.dart
@@ -1,3 +1,5 @@
+import 'package:cookbook/src/components/btn_border_radius.dart';
+import 'package:cookbook/src/components/input.dart';
 import 'package:flutter/material.dart';
 
 class Settings extends StatefulWidget {
@@ -9,14 +11,23 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: SingleChildScrollView(
+        body: Center(
+      child: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text(" :)")],
+          children: [
+            CircleAvatar(
+                radius: 50.0,
+                backgroundImage: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA5LQ0xx-RV4HDwd-sRI7pzTDRDY4pMTwI7g&usqp=CAU')),
+            InputForm('Nome', Icons.person),
+            InputForm('Email', Icons.alternate_email),
+            InputForm('Senha', Icons.lock),
+            BotaoGradiente('Salvar', '/home'),
+          ],
         ),
       ),
-    );
+    ));
   }
 }
